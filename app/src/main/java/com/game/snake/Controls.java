a4ac16118ef55c3e5a2df8061a3f36997fd69014
VishwarajAnand <vishwaraj.anand00@hotmail.com>
2017-10-02 01:55:31 +0800

    Added controls but not working!

---
+++ app/src/main/java/com/game/snake/Controls.java
@@ -0,0 +1,11 @@
+package com.game.snake;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+/**
+ * Created by dabba on 2/10/17.
+ */
+
--- app/src/main/java/com/game/snake/home.java
+++ app/src/main/java/com/game/snake/home.java
@@ -2,6 +2,10 @@ package com.game.snake;
 
 import android.content.Context;
 import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -9,13 +13,28 @@ import android.view.Display;
 import android.view.WindowManager;
 import android.widget.TextView;
 
-public class home extends AppCompatActivity {
+import java.util.Arrays;
+
+public class home extends AppCompatActivity implements SensorEventListener{
+    // Start with some variables
+    private SensorManager sensorMan;
+    private Sensor rotationSensor;
+    private final float[] mRotationMatrix = new float[16];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         displayText(originalScreenSize());
+
+        // load sensors information
+        sensorMan = (SensorManager) getSystemService(SENSOR_SERVICE);
+        rotationSensor = sensorMan.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
+        // initialize the rotation matrix to identity
+        mRotationMatrix[ 0] = 1;
+        mRotationMatrix[ 4] = 1;
+        mRotationMatrix[ 8] = 1;
+        mRotationMatrix[12] = 1;
     }
 
     private void displayText(String textData){
@@ -27,11 +46,31 @@ public class home extends AppCompatActivity {
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getRealSize(size);
-//        DisplayMetrics metrics = new DisplayMetrics();
-//        WindowManager windowManager = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
-//        Display defaultDisplay = windowManager.getDefaultDisplay();
-//        defaultDisplay.getRealMetrics(metrics);
-//        return String.valueOf("Original screen size: " + metrics.widthPixels) + "X" + String.valueOf(metrics.heightPixels);
         return String.valueOf("Original screen size: " + size.x) + "X" + String.valueOf(size.y);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_GAME_ROTATION_VECTOR) {
+            SensorManager.getRotationMatrixFromVector(mRotationMatrix , event.values);
+        }
+        displayText(Arrays.toString(mRotationMatrix));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // required method
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorMan.registerListener(this, rotationSensor, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorMan.unregisterListener(this);
     }
 }

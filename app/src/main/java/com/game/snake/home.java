9cb95ba81713881cdf42ee433a722e737aa1fde5
VishwarajAnand <vishwaraj.anand00@hotmail.com>
2017-10-02 00:12:53 +0800

    Got 1080x1920 as original display size from current code.

--- app/src/main/java/com/game/snake/home.java
+++ app/src/main/java/com/game/snake/home.java
@@ -1,7 +1,12 @@
 package com.game.snake;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class home extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        displayText(originalScreenSize());
+    }
+
+    private void displayText(String textData){
+        TextView textViewScreenSize = (TextView) findViewById(R.id.textViewScreenSize);
+        textViewScreenSize.setText(textData);
+    }
+
+    private String originalScreenSize() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        Display defaultDisplay = windowManager.getDefaultDisplay();
+        defaultDisplay.getRealMetrics(metrics);
+        return String.valueOf("Original screen size: " + metrics.widthPixels) + "X" + String.valueOf(metrics.heightPixels);
     }
 }
